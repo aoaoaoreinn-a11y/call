08e7949312282b9b0ee3e8b29a5ab58c30a00701
aoaoaoreinn-a11y <aoaoaoreinn@gmail.com>
2026-08-23 17:16:12 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getDatabase
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+
+const firebaseConfig = {
+  databaseURL:
+    "https://あなたのDB.firebaseio.com"
+};
+
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+export const database =
+  getDatabase(app);
